1aba3e11817d1987eae5f84283ec106b14c6d614
Sam Cho <samcho0608@g.ucla.edu>
2021-04-29 17:20:13 +0900

    task 2 finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function favSnack() {
-  return 'Milk Cream Bread';
+function Food(props) {
+  const [likes, setLikes] = useState(0);
+  const [selfCount, setSelfCount] = useState(1);
+  
+  const makeIndexArray = (len) => {
+    const indices = [];
+    for(var i = 0; i < len; i++) indices.push(i);
+    return indices;
+  }
+  // single tags like take the form of <tagName/>
+  return (
+    <>
+      {makeIndexArray(selfCount).map((index) => {
+        return (
+          <div className='Food' id={props.name}>
+            {props.name}<br/>{likes} likes<br/>
+            <button class='likeBtn' onClick={() => setLikes(likes + 1)}>Like Me</button>
+            <br/>
+            <button class='rmBtn' onClick={() => setSelfCount(selfCount - 1)}>Remove Me</button>
+            <br/>
+            <button class='dupBtn' onClick={() =>setSelfCount(selfCount + 1)}>Duplicate Me</button>
+          </div>
+        )
+      })}
+    </>
+  );
 }
 
 function App() {
-  const greeting = "Hello";
+  const [foods] = useState(['Donkatsu', 'Curry', 'Hamburger']);
+  // <> called fragment aka grouping bunch of tags in a group
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-	  <h1>{favSnack()}</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          foods.map((food) => <>
+          <Food key={food} name={food}/>
+          <br/>
+          </>
+          )
+        }
       </header>
     </div>
   );
